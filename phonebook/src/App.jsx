005698c120a5b9f1bb13d83a3cc75c2540175bfe
congbk92 <thanhcong.bk10@gmail.com>
2025-03-19 22:31:50 +0700

    Exercise 2.11.

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filter, onChange}) => {
   return (
@@ -33,16 +34,20 @@ const Persons = ({persons}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }  
-  ])
+  const [persons, setPersons] = useState([])
   const [showPersons, setShowPersons] = useState(persons)
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameFilter, setNameFilter] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      console.log(response.data)
+      setPersons(response.data)
+      setShowPersons(getShowPersons(response.data, nameFilter))
+    })
+  }, [])
 
   const getShowPersons = (persons, filter) => {
     let ret = persons
